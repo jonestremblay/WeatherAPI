8b748ce8ef378e97f662c12810066e2efc2755f1
Jonathan Tremblay <jonathanrtremblay@gmail.com>
2023-07-10 13:17:56 -0400

    init commit

---
+++ src/main/java/ca/jonestremblay/WeatherAPI/WeatherApiApplication.java
@@ -0,0 +1,14 @@
+package ca.jonestremblay.WeatherAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherApiApplication.class, args);
+		System.out.println("allo");
+	}
+
+}
---
+++ src/test/java/ca/jonestremblay/WeatherAPI/WeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jonestremblay.WeatherAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
